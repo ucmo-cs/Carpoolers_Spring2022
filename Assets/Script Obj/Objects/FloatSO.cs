e430799468633b4c1f717c089bc30c2dde824586
Nate <nate13nate@gmail.com>
2022-01-31 19:04:52 -0600

    Forgot to add line that made object an option in menu

--- Assets/Script Obj/Objects/FloatSO.cs
+++ Assets/Script Obj/Objects/FloatSO.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "Variables/Float")]
 public class FloatSO : ScriptableObject
 {
   public float value;
